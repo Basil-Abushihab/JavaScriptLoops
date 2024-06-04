3040df9fc5c61f1b0e81b9ae91b568f7d5ee73d3
Basil <basilabushihab22@gmail.com>
2024-06-04 16:34:51 +0300

    added loops.js

---
+++ loops.js
@@ -0,0 +1,36 @@
+for (let i = 0; i <= 5; i++) {
+  alert(i);
+}
+let str = "";
+for (let i = 0; i <= 5; i++) {
+  str += i.toString();
+}
+alert(str);
+
+for (let i = 0; i <= 20; i = i * 3) {
+  if (i == 0) {
+    console.log(i);
+    i++;
+    continue;
+  } else console.log(i);
+}
+
+while (true) {
+  let val = prompt("please Enter a value");
+  let parsedVal = parseInt(val, 10);
+  if (parsedVal > 100 || parsedVal < 0) alert("Please re-enter a value");
+  else {
+    alert("Your number is " + parsedVal);
+    break;
+  }
+}
+
+let val = parseInt(
+  prompt("Enter a value to return the sum of all the numbers before it"),
+  10
+);
+let sum = 0;
+for (let i = 0; i <= val; i++) {
+  sum += i;
+}
+console.log(sum);
